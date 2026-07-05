300f5b256eec1557cf9a01d197df36793932090c
yalamakuriarchana <yalamakuriarchana@gmail.com>
2026-07-05 19:33:34 +0530

    Fixed Customer model

--- server/models/Customer.js
+++ server/models/Customer.js
@@ -1,30 +1,21 @@
 const mongoose = require("mongoose");
 
-const customerSchema = new mongoose.Schema(
-{
-    name: {
-        type: String,
-        required: true
-    },
-
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
-    phone: {
-        type: String,
-        required: true
-    },
-
-    address: {
-        type: String,
-        required: true
-    }
-},
-{
-    timestamps: true
+const CustomerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String
+  },
+  address: {
+    type: String
+  }
 });
 
-module.exports = mongoose.model("Customer", customerSchema);
+module.exports = mongoose.model("Customer", CustomerSchema);
